036588f481a8dafd8f15e77e4097ce1369e2e5eb
Koray Dundar <koray.dundar@exove.com>
2023-02-22 16:25:38 +0200

    elephant sql and sequelize connections

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express ()
-const mustache = require('mustache')
+const mustacheExpress = require('mustache-express')
 const path = require('path')
 
 const PORT = 3000
 const VIEWS_PATH = path.join(__dirname, '/views')
 
-app.engine('mustache', mustacheExpress(VIEWS_PATH + 'partials/','.mustache'))
+app.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials','.mustache'))
 app.set('views',VIEWS_PATH)
 app.set('view engine','mustache')
+
+// Register
+app.get('/register',(req,res) => {
+    res.render('register')
+})
+
+// Login
+app.get('/login',(req,res) => {
+    res.render('login')
+})
 
 app.listen(PORT, () => console.log('Server is running.'))
