61bfdb1b55bb3591176a42c13887e891efc46a0b
Koray Dundar <koray.dundar@exove.com>
2023-02-23 13:06:11 +0200

    implemented login functionality

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const models = require('./models')
 const bcrypt = require('bcrypt')
+const session = require('express-session')
 
 const SALT_ROUNDS = 10;
 const PORT = 3000
 const VIEWS_PATH = path.join(__dirname, '/views')
+
+app.use(session({
+    secret: 'somesecret',
+    resave: true,
+    saveUninitialized: false
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -29,6 +36,36 @@ app.get('/login',(req,res) => {
 })
 
 // POST METHOD
+app.post('/login', async (req,res) => {
+    // get username and password from form inputs
+    let username = req.body.username
+    let password = req.body.password
+
+    // check if the username exists in database
+    let user = await models.User.findOne({
+        where: {
+            username: username
+        }
+    })
+
+    // if user exists
+    if(user != null){
+        bcrypt.compare(password, user.password,(error, result) => {
+            if(result) {
+                //create a session
+                if(req.session) {
+                    req.session.user = {userId: user.id}
+                    res.redirect('user/products')
+                }
+            } else {
+                res.render('/login', {message: 'Incorrect username or password'})
+            }
+        })
+    } else {
+        res.render('/login', {message: 'Username or password is invalid!'})
+    }
+})
+
 app.post('/register',async (req,res) => {
     let username = req.body.username
     let password = req.body.password
@@ -45,7 +82,7 @@ app.post('/register',async (req,res) => {
 
         bcrypt.hash(password, SALT_ROUNDS, async (error, hash) => {
             if(error) {
-                res.render('/password', {message: 'Error occured when registering'})
+                res.render('/register', {message: 'Error occured when registering'})
             } else {
                 let user = models.User.build({
                     username : username,
