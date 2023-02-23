34975d5e0b7c9be390227eaff4560873db90dc39
Koray Dundar <koray.dundar@exove.com>
2023-02-23 16:46:32 +0200

    refactored the code

--- routes/index.js
+++ routes/index.js
@@ -3,89 +3,91 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const models = require('../models')
 
-const SALT_ROUNDS = 10;
+const SALT_ROUNDS = 10
 
-// GET METHOD
-
-// Register
-router.get('/register',(req,res) => {
-    res.render('register')
-})
-
-// Login
 router.get('/login',(req,res) => {
-    res.render('login')
+  res.render('login')
 })
 
-// POST METHOD
-router.post('/login', async (req,res) => {
-    // get username and password from form inputs
-    let username = req.body.username
-    let password = req.body.password
-
-    // check if the username exists in database
-    let user = await models.User.findOne({
-        where: {
-            username: username
-        }
-    })
-
-    // if user exists
-    if(user != null){
-        bcrypt.compare(password, user.password,(error, result) => {
-            if(result) {
-                //create a session
-                if(req.session) {
-                    req.session.user = {userId: user.id}
-                    res.redirect('user/products')
-                }
-            } else {
-                res.render('/login', {message: 'Incorrect username or password'})
-            }
-        })
-    } else {
-        res.render('/login', {message: 'Username or password is invalid!'})
-    }
+router.get('/register',(req,res) => {
+  res.render('register')
 })
 
 router.post('/register',async (req,res) => {
-    let username = req.body.username
-    let password = req.body.password
 
-    // check if the user exist
-    let persistedUser = await models.User.findOne({
-        where: {
-            username: username
-        }
-    })
+  let username = req.body.username
+  let password = req.body.password
 
-    // if user does not exist creat one
-    if(persistedUser == null){
+  let persistedUser = await models.User.findOne({
+    where: {
+      username: username
+    }
+  })
 
-        bcrypt.hash(password, SALT_ROUNDS, async (error, hash) => {
-            if(error) {
-                res.render('/register', {message: 'Error occured when registering'})
-            } else {
-                let user = models.User.build({
-                    username : username,
-                    password: hash
-                })
+  if(persistedUser == null) {
 
-                let savedUser = await user.save();
+    bcrypt.hash(password, SALT_ROUNDS, async (error, hash) => {
 
-                // if user exists redirect them to login page
-                if(savedUser != null) {
-                    res.redirect('/login')
-                } else {
-                    res.render('/register', {message: "User already exists!"})
-                }
-            }
+      if(error) {
+        res.render('/register',{message: 'Error creating user!'})
+      } else {
+
+        let user = models.User.build({
+          username: username,
+          password: hash
         })
 
-    } else {
-        res.render('/register', {message: "User already exists!"})
-    }
+        let savedUser = await user.save()
+        if(savedUser != null) {
+          res.redirect('/login')
+        } else {
+          res.render('/register',{message: "User already exists!"})
+        }
+
+      }
+
+    })
+
+  } else {
+      res.render('/register',{message: "User already exists!"})
+  }
+
 })
 
-module.exports = router
+router.post('/login', async (req,res) => {
 
+  let username = req.body.username
+  let password = req.body.password
+
+  let user = await models.User.findOne({
+    where: {
+      username: username
+    }
+  })
+
+  if(user != null) {
+
+    bcrypt.compare(password, user.password,(error, result) => {
+
+      if(result) {
+
+        // create a session
+        if(req.session) {
+          req.session.user = {userId: user.id}
+          res.redirect('/users/products')
+        }
+
+      } else {
+        res.render('login',{message: 'Incorrect username or password'})
+      }
+
+    })
+
+  } else { // if the user is null
+    res.render('login',{message: 'Incorrect username or password'})
+  }
+
+})
+
+
+module.exports = router
