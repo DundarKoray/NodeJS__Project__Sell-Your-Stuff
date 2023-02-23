1ecf781617293c2911afd4cac26d4e80dd08dec6
Koray Dundar <koray.dundar@exove.com>
2023-02-23 10:53:42 +0200

    implementing user registration

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const app = express ()
 const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
 const path = require('path')
+const models = require('./models')
 
 const PORT = 3000
 const VIEWS_PATH = path.join(__dirname, '/views')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials','.mustache'))
 app.set('views',VIEWS_PATH)
 app.set('view engine','mustache')
+
+// GET METHOD
 
 // Register
 app.get('/register',(req,res) => {
@@ -19,5 +25,38 @@ app.get('/register',(req,res) => {
 app.get('/login',(req,res) => {
     res.render('login')
 })
+
+// POST METHOD
+app.post('/register',async (req,res) => {
+    let username = req.body.username
+    let password = req.body.password
+
+    // check if the user exist
+    let persistedUser = await models.User.findOne({
+        where: {
+            username: username
+        }
+    })
+
+    // if user does not exist creat one
+    if(persistedUser == null){
+        let user = models.User.build({
+            username : username,
+            password: password
+        })
+
+        let savedUser = await user.save();
+
+        // if user exists redirect them to login page
+        if(savedUser != null) {
+            res.redirect('/login')
+        } else {
+            res.render('/register', {message: "User already exists!"})
+        }
+    } else {
+        res.render('/register', {message: "User already exists!"})
+    }
+})
+
 
 app.listen(PORT, () => console.log('Server is running.'))
