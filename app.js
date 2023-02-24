47f93427e9871a4044114d3d58b4159500c058d3
Koray Dundar <koray.dundar@exove.com>
2023-02-24 10:29:31 +0200

    uploading image file

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+
 const express = require('express')
-const app = express ()
+const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const models = require('./models')
 const session = require('express-session')
 const indexRoutes = require('./routes/index')
 const userRoutes = require('./routes/users')
 
 const PORT = 3000
-const VIEWS_PATH = path.join(__dirname, '/views')
+const VIEWS_PATH = path.join(__dirname,'/views')
+
+global.__basedir = __dirname
 
 app.use(session({
-    secret: 'somesecret',
-    resave: true,
-    saveUninitialized: false
+  secret: 'somesecret',
+  resave: true,
+  saveUninitialized: false
 }))
+
+// static folder
+app.use('/uploads',express.static('uploads'))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -25,4 +32,4 @@ app.set('view engine','mustache')
 app.use('/',indexRoutes)
 app.use('/users',userRoutes)
 
-app.listen(PORT, () => console.log('Server is running.'))
+app.listen(PORT,() => console.log('Server is running...'))
