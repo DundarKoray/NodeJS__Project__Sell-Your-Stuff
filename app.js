3628e58149844d854b8fb1b0e3fdbd49414b59ec
Koray Dundar <koray.dundar@exove.com>
2023-02-28 15:01:48 +0200

    protected routhes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const models = require('./models')
 const session = require('express-session')
 const indexRoutes = require('./routes/index')
 const userRoutes = require('./routes/users')
+const checkAuthorization = require('./middlewares/authorization')
 
 const PORT = 3000
 const VIEWS_PATH = path.join(__dirname,'/views')
@@ -32,6 +33,6 @@ app.set('views',VIEWS_PATH)
 app.set('view engine','mustache')
 
 app.use('/',indexRoutes)
-app.use('/users',userRoutes)
+app.use('/users',checkAuthorization,userRoutes)
 
 app.listen(PORT,() => console.log('Server is running...'))
---
+++ middlewares/authorization.js
@@ -0,0 +1,11 @@
+function checkAuthorization (req, res, next) {
+    if(req.session) {
+        if(req.session.user) {
+            next()
+        } else {
+            res.redirect('/login')
+        }
+    }
+}
+
+module.exports = checkAuthorization;
